8e51cf83d584fd319ce03f10edf0aa28e3de0da7
Ziek <zieksanchez3@gmail.com>
2025-04-30 16:23:52 -0700

    Add workflow

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/api/cats", (req, res) => {
-  res.send("Successful Response");
+  res.send("New Successful Response");
 });
 
 app.listen(3000, () => console.log("Example app is listening on port 3000"));
