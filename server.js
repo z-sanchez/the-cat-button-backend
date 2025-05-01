f3aceddc5df5fc75e886620a92617ee4fbd012e2
Ziek <zieksanchez3@gmail.com>
2025-05-01 15:32:47 -0700

    Add test message

--- server.js
+++ server.js
@@ -10,4 +10,8 @@ app.get("/cats", (req, res) => {
   res.send("Cats Response Path");
 });
 
+app.get("/", (req, res) => {
+  res.send("You have reached the cat button server");
+});
+
 app.listen(3000, () => console.log("Example app is listening on port 3000"));
